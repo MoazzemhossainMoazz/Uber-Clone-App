3b077bc4d40b80c6f9a763bee8750c0c887a73ba
Moazz <moazzemh222@gmail.com>
2022-12-08 00:03:11 +0600

    Tuto-5th complete

--- lib/splashScreen/splash_screen.dart
+++ lib/splashScreen/splash_screen.dart
@@ -1,5 +1,6 @@
 import 'dart:async';
 
+import 'package:drivers_app/authentication/signup_screen.dart';
 import 'package:drivers_app/mainScreens/main_screen.dart';
 import 'package:flutter/material.dart';
 
@@ -20,10 +21,10 @@ class _MySplashScreenState extends State<MySplashScreen>
 
   startTimer()
   {
-    Timer(const Duration(seconds: 13), () async
+    Timer(const Duration(seconds: 3), () async
     {
       //send user to main screen
-      Navigator.push(context, MaterialPageRoute(builder: (c)=>const MainScreen()));
+      Navigator.push(context, MaterialPageRoute(builder: (c)=> SignUpScreen()));
     });
   }
 
